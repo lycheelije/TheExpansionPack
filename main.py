0d60af6a19c97df42e2dfa87fcb8e1a968029d66
Edward Bruin <edwardnbruin@gmail.com>
2023-09-29 03:19:40 +1000

    Further integrated more of Tylar's code, mostly into the notebook. included an example dataset

--- main.py
+++ main.py
@@ -39,12 +39,6 @@ def predict_response(message, prompt, model_id, api_token):
 	API_URL = f"https://api-inference.huggingface.co/models/{model_id}"
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-    
-def calculate_prompt(message, response):
-    # Tylar's method of reverse-engineering the response to get the prompt
-    # this will be used in the training process
-    prompt = 'placeholder'
-    return(prompt)
 
 #Function to calculate uniqueness score
 def calculate_word_uniqueness(word):
@@ -65,34 +59,16 @@ def calculate_prompt(message, response, limit = 1):
     # Find prompt words using word subtraction
     prompt_words = list(set([token.text for token in response_tokens]) - set([token.text for token in message_tokens]) - set([token.text for token in response_tokens if "'" in token.text]))
 
-    # Initialize variables to keep track of the most unique word and its score for this prompt
-    most_unique_word = None
-    max_uniqueness_score = -1  # Initialize with a very low value
-
-    # Remove punctuation from the list of prompt words using the string module
-    # cleaned_prompt_words = [word.translate(str.maketrans('', '', string.punctuation)) for word in prompt_words]
-    # print(prompt_words)
-    # print(cleaned_prompt_words)
-    # Calculate the uniqueness score (cosine similarity) for each non-stopword word in the list
+    #results = sorted(prompt_words, key=calculate_word_uniqueness, reverse=False)
     results = sorted(prompt_words, key=calculate_word_uniqueness, reverse=True)
     return(results[:limit])
-    
-    #for word in prompt_words:
-        # Check if the word is not empty after removing punctuation
-    #    if word.strip() and word not in stopwords:
-    #        uniqueness_score = calculate_word_uniqueness(word)
-            
-            # Check if this word is more unique than the previous most unique word
-    #        if uniqueness_score > max_uniqueness_score:
-    #            max_uniqueness_score = uniqueness_score
-    #            most_unique_word = word
-    
-    #return (prompt_words, most_unique_word)   
+
 
 def calculate_prompts(input_file_path):
 
-    print("loading parameters")
     # Load a pre-trained spaCy model with word embeddings (e.g., en_core_web_md)
+    
+    print("loading parameters")
     global nlp
     nlp = spacy.load("en_core_web_md")
     
@@ -116,6 +92,7 @@ def calculate_prompts(input_file_path):
         # Store the result for this section in the dictionary
         # input_data[i].prompt = prompt_words
         working_data[i]['prompt'] = prompt_words
+    print("complete")
     return (working_data)
 
 def save_json(output_data, output_file_path="uniqueness_scores.json"):
